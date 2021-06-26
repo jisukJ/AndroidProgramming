083ea99f8ec7a1c325f96abaa0c6ee1a94bb6da7
jisukJ <78594351+jisukJ@users.noreply.github.com>
2021-06-26 17:57:27 +0900

    last commit of MVVM test app

--- BeatBox/app/src/main/java/com/example/beatbox/BeatBox.kt
+++ BeatBox/app/src/main/java/com/example/beatbox/BeatBox.kt
@@ -1,17 +1,33 @@
 package com.example.beatbox
 
 import android.content.res.AssetManager
+import android.media.SoundPool
 import android.util.Log
+import java.io.IOException
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
+private const val MAX_SOUNDS = 5
 
 class BeatBox(private val assets: AssetManager) {
 
     val sounds: List<Sound>
+    private val soundPool=SoundPool.Builder()
+        .setMaxStreams(MAX_SOUNDS)
+        .build()
 
     init {
         sounds = loadSounds()
+    }
+
+    fun play(sound:Sound){
+        sound.soundId?.let{
+            soundPool.play(it,1.0f,1.0f,1,0,1.0f)
+        }
+    }
+
+    fun release(){
+        soundPool.release()
     }
 
     private fun loadSounds(): List<Sound> {
@@ -24,10 +40,21 @@ class BeatBox(private val assets: AssetManager) {
         }
         val sounds = mutableListOf<Sound>()
         soundNames.forEach { filename ->
-            val assetPath="$SOUNDS_FOLDER/$filename"
-            val sound=Sound(assetPath)
-            sounds.add(sound)
+            val assetPath = "$SOUNDS_FOLDER/$filename"
+            val sound = Sound(assetPath)
+            try{
+                load(sound)
+                sounds.add(sound)
+            }catch (ioe:IOException){
+                Log.d(TAG,"Cound not load sound $filename",ioe)
+            }
         }
         return sounds
+    }
+
+    private fun load(sound:Sound){
+        val afd=assets.openFd(sound.assetPath)
+        val soundId=soundPool.load(afd,1)
+        sound.soundId=soundId
     }
 }
